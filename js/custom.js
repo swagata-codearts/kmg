682287b4cde726b4496f59bfb0454db93f7fce5e
swagata-codearts <swagata.codearts@gmail.com>
2022-09-02 10:20:53 +0530

    changes on product pages

--- js/custom.js
+++ js/custom.js
@@ -16,6 +16,23 @@ jQuery('#quick-view-owl').owlCarousel({
     },
     navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
 });
+jQuery('#slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    },
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+});
 
 jQuery(document).ready(function(){
     jQuery(window).scroll(function() {
